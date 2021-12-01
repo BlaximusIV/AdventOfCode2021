6c62ddff55c77f3a71551d848102e87e92cbf735
Neal Darley <darley.neal@gmail.com>
2021-11-30 23:59:13 -0700

    Add d1 pt 1, git ignore

---
+++ Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿var depthReadings = new List<int>();
+
+// Change for appropriate path
+const string inputPath = @"C:\repos\AdventOfCode2021\Day1\input.txt";
+using (var sr = new StreamReader(inputPath))
+    while (!sr.EndOfStream)
+        depthReadings.Add(int.Parse(sr.ReadLine()));
+
+int? cursor = null;
+var depthIncreaseCount = 0;
+
+foreach(var reading in depthReadings)
+{
+    if (reading > cursor)
+        depthIncreaseCount++;
+
+    cursor = reading;
+}
+
+Console.WriteLine($"{depthIncreaseCount} increasing readings");
