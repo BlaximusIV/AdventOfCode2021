b3e3343123fd1b9a1eef9a06ae8f6275cf7fe640
Neal Darley <darley.neal@gmail.com>
2021-12-03 00:28:24 -0700

    Day3, pt1

---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,36 @@
+﻿
+var binaryStrings = new List<string>();
+
+// Change for appropriate path
+const string inputPath = @"C:\repos\AdventOfCode2021\Day3\input.txt";
+using (var sr = new StreamReader(inputPath))
+    while (!sr.EndOfStream)
+        binaryStrings.Add(sr.ReadLine());
+
+var gammaCounter = new int[12];
+foreach(var line in binaryStrings)
+{
+    for(int i = 0; i < line.Length; i++)
+        if(line[i] == '1')
+            gammaCounter[i]++;
+}
+
+var gammaBinaryString = string.Empty;
+var epsilonString = string.Empty;
+for (int i = 0; i < gammaCounter.Length; i++)
+    // We know the length of the list, so it's easy to tell if it is in the majority
+    if (gammaCounter[i] > 500)
+    {
+        gammaBinaryString += "1";
+        epsilonString += "0";
+    }
+    else
+    {
+        gammaBinaryString += "0";
+        epsilonString += "1";
+    }
+
+var powerConsumption = Convert.ToInt32(gammaBinaryString, 2) * Convert.ToInt32(epsilonString, 2);
+
+Console.WriteLine($"Power consumption: {powerConsumption} units");
+
