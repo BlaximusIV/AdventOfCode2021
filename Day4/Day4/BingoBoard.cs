a86190736e669d86fc344751edf1f53badc4d0e6
Neal Darley <darley.neal@gmail.com>
2021-12-03 23:56:04 -0700

    Partial Day4, pt 1

---
+++ Day4/Day4/BingoBoard.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    public class BingoBoard
+    {
+
+        private bool[][] board = new bool[5][];
+
+        private readonly Dictionary<string, (int x, int y)> boardMap = new Dictionary<string, (int, int)>();
+
+        public BingoBoard(string[] boardRows)
+        {
+            for (int i = 0; i < boardRows.Count(); i++)
+            {
+                var rowNumbers = boardRows[i].Split(' ');
+                for (int j = 0; j < rowNumbers.Length; j++)
+                    boardMap[rowNumbers[j]] = (i, j);
+            }
+        }
+
+        public void MarkBoard(string number)
+        {
+            if (boardMap.ContainsKey(number))
+                board[boardMap[number].x][boardMap[number].y] = true;
+        }
+
+        public bool IsWinningBoard()
+        {
+            // We know ahead of time that we will only have 5 X 5 boards,
+            // so we can be more relaxed about indexes
+            for (int i = 0;i < board.Length; i++)
+                if (IsWinningRow(board[i]))
+                    return true;
+
+            for (int i = 0; i <= board.Length; i++)
+                if (IsWinningColumn(i))
+                    return true;
+
+            return false;
+        }
+
+        public long FindWinningScore(string winningNumber)
+        {
+            var tally = 0;
+            foreach (var key in boardMap.Keys)
+            {
+                // If the position has been chosen
+                if (board[boardMap[key].x][boardMap[key].y])
+                    continue;
+
+                tally += int.Parse(key);
+            }
+
+            return tally * int.Parse(winningNumber);
+        }
+
+        private bool IsWinningRow(bool[] row) => row.All(position => position is true);
+
+        private bool IsWinningColumn(int columnIndex)
+        {
+            var pickedCount = 0;
+            foreach (var row in board)
+                if (row[columnIndex])
+                    pickedCount++;
+
+            return pickedCount == board.Length;
+        }
+
+    }
+}
---
+++ Day4/Day4/BingoSimulator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    public class GameExtractor
+    {
+        private readonly List<string> pickedNumbers;
+
+        private readonly List<BingoBoard> bingoBoards;
+
+        public BingoSimulator(List<string> input)
+        {
+            for (int i = 0; i < input.Count; i++)
+            {
+                bool isBoard = false;
+                if (i == 0)
+                    pickedNumbers = input[i].Split(',').ToList();
+
+                if ()
+            }
+        }
+
+        public long FindWinningScore()
+        {
+            for (int i = 0; i < pickedNumbers.Count; i++)
+                foreach(var board in bingoBoards)
+                {
+                    board.MarkBoard(pickedNumbers[i]);
+
+                    // Small optimization, no use checking if we can't possibly have a winner yet
+                    const int MinimumWinPickCount = 5;
+                    if (i >= MinimumWinPickCount - 1 && board.IsWinningBoard())
+                        return board.FindWinningScore(pickedNumbers[i]);
+                }
+
+            throw new Exception("No winner found with the given list of picked numbers");
+        }
+
+    }
+}
