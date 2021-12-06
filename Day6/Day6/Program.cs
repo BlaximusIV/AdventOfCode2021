bab6dc439b43e63aa1284baaac87a1f0599ea8fb
Neal Darley <darley.neal@gmail.com>
2021-12-05 23:10:01 -0700

    D6, pt 2. More performant. Seems like there should be slicker ways to do it.

--- Day6/Day6/Program.cs
+++ Day6/Day6/Program.cs
@@ -6,24 +6,33 @@ const string inputPath = @"C:\repos\AdventOfCode2021\Day6\input.txt";
 using (var sr = new StreamReader(inputPath))
     fishAges.AddRange(sr.ReadLine().Split(',').Select(s => int.Parse(s)));
 
-const int iterationCount = 80;
+
+Dictionary<int, double> FishAtDays = new Dictionary<int, double>()
+{
+    { 0, 0 },
+    { 1, 0 },
+    { 2, 0 },
+    { 3, 0 },
+    { 4, 0 },
+    { 5, 0 },
+    { 6, 0 },
+    { 7, 0 },
+    { 8, 0 }
+};
+
+foreach (var age in fishAges)
+    FishAtDays[age]++;
+
+const int iterationCount = 256;
 for (int i = 0; i < iterationCount; i++)
 {
-    var newFish = new List<int>();
-    for (int j = 0; j < fishAges.Count; j++)
-    {
-        if (fishAges[j] == 0)
-        {
-            fishAges[j] = 6;
-            newFish.Add(8);
-        }
-        else
-        {
-            fishAges[j]--;
-        }
-    }
+    double buffer = FishAtDays[0];
 
-    fishAges.AddRange(newFish);
+    for (int j = 0; j < FishAtDays.Count - 1; j++)
+        FishAtDays[j] = FishAtDays[j + 1];
+
+    FishAtDays[6] += buffer;
+    FishAtDays[8] = buffer;
 }
 
-Console.WriteLine($"Total fish after {iterationCount} days: {fishAges.Count}");
+Console.WriteLine($"Total fish after {iterationCount} days: {FishAtDays.Values.Sum()}");
