eddee34f093b7370dde5e5f4b92a7c622db0773d
Neal Darley <darley.neal@gmail.com>
2021-12-06 23:13:41 -0700

    A large performance improvement for implementation of d7, p2

--- Day7/Day7/Program.cs
+++ Day7/Day7/Program.cs
@@ -7,16 +7,20 @@ using (var sr = new StreamReader(inputPath))
     subPositions.AddRange(sr.ReadLine().Split(',').Select(s => int.Parse(s)));
 
 // Create fuel map to calculate once... It seems like there should be a fibonacci or bit manipulation something for this, but whatever
-var fuelMap = new List<int>();
-for (int i = 0; i < subPositions.Max(); i++)
-    fuelMap.Add(i);
+var fuelIncreaseByDistance = new List<int>();
+var fuelMap = new Dictionary<int, long>();
+for (int i = 0; i < subPositions.Max() + 1; i++)
+{
+    fuelIncreaseByDistance.Add(i);
+    fuelMap[i] = fuelIncreaseByDistance.Sum();
+}
 
-var minimumTotalFuel = int.MaxValue;
+var minimumTotalFuel = long.MaxValue;
 for (int i = 0; i < subPositions.Max(); i++)
 {
-    var totalFuel = 0;
+    long totalFuel = 0;
     foreach (var subPosition in subPositions)
-        totalFuel += fuelMap.Where(d => d <= Math.Abs(subPosition - i)).Sum();
+        totalFuel += fuelMap[Math.Abs(subPosition - i)];
 
     if (totalFuel < minimumTotalFuel)
         minimumTotalFuel = totalFuel;
