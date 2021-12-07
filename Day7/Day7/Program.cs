a992d2722a9f9e441186a22ec16a9c29b71ee1e0
Neal Darley <darley.neal@gmail.com>
2021-12-06 23:04:45 -0700

    D7, pt 2.... I'm sure there's a much more elegant way to do it.

--- Day7/Day7/Program.cs
+++ Day7/Day7/Program.cs
@@ -6,12 +6,17 @@ const string inputPath = @"C:\repos\AdventOfCode2021\Day7\input.txt";
 using (var sr = new StreamReader(inputPath))
     subPositions.AddRange(sr.ReadLine().Split(',').Select(s => int.Parse(s)));
 
+// Create fuel map to calculate once... It seems like there should be a fibonacci or bit manipulation something for this, but whatever
+var fuelMap = new List<int>();
+for (int i = 0; i < subPositions.Max(); i++)
+    fuelMap.Add(i);
+
 var minimumTotalFuel = int.MaxValue;
 for (int i = 0; i < subPositions.Max(); i++)
 {
     var totalFuel = 0;
     foreach (var subPosition in subPositions)
-        totalFuel += Math.Abs(subPosition - i);
+        totalFuel += fuelMap.Where(d => d <= Math.Abs(subPosition - i)).Sum();
 
     if (totalFuel < minimumTotalFuel)
         minimumTotalFuel = totalFuel;
